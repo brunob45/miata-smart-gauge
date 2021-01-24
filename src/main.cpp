7ae657495e00b04a084107f3e4e318d7256e40d5
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2021-01-24 00:03:27 -0500

    don't use return mid-function

--- src/main.cpp
+++ src/main.cpp
@@ -25,22 +25,22 @@ void update_values()
     static uint32_t last_update = 0;
     const uint32_t now = millis();
 
-    if (now - last_update < 100)
-        return;
-
-    GV.rpm += rpm_inc;
-    if (GV.rpm >= 8000 && rpm_inc > 0)
+    if (now - last_update >= 100)
     {
-        rpm_inc = -200;
+        last_update = now;
+
+        GV.rpm += rpm_inc;
+        if (GV.rpm >= 8000 && rpm_inc > 0)
+        {
+            rpm_inc = -200;
+        }
+        else if (GV.rpm <= 200 && rpm_inc < 0)
+        {
+            rpm_inc = 100;
+        }
+
+        GV.current_gear = GV.rpm / 1335;
+
+        GV.alert = GV.rpm >= 7200;
     }
-    else if (GV.rpm <= 200 && rpm_inc < 0)
-    {
-        rpm_inc = 100;
-    }
-
-    GV.current_gear = GV.rpm / 1335;
-
-    GV.alert = GV.rpm >= 7200;
-
-    last_update = now;
 }

b165931bba62e1afe59eae90d63fb97373609ff3
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2022-03-19 14:10:07 -0400

    automatically copy lv_conf.h

--- scripts/pre_build.py
+++ scripts/pre_build.py
@@ -1,8 +1,11 @@
 import platform
+import shutil
 
 Import('env')
 
 if platform.system() == 'Windows':
     env.Execute('wsl sh scripts/get_sha.sh')
-else: # Linux
+else:  # Linux
     env.Execute('sh scripts/get_sha.sh')
+
+shutil.copyfile('include/lv_conf.h', 'lib/lvgl/lv_conf.h')

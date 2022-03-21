0cf6ec98c3e9235d9641515df6b0c425e392f3c2
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2022-03-20 20:59:55 -0400

    remove bg.h

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -360,7 +360,7 @@
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
  *E.g. #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)*/
-#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(din1451_18) LV_FONT_DECLARE(din1451_14) 
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(din1451_18) LV_FONT_DECLARE(din1451_14)
 
 /*Always set a default font*/
 #define LV_FONT_DEFAULT &lv_font_montserrat_14

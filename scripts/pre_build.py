d04032933c066a009327d2fcf1cab3e86cc1c809
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2022-03-20 21:23:50 -0400

    make lvgl a platformio library

--- scripts/pre_build.py
+++ scripts/pre_build.py
@@ -7,5 +7,3 @@ if platform.system() == 'Windows':
     env.Execute('wsl sh scripts/get_sha.sh')
 else:  # Linux
     env.Execute('sh scripts/get_sha.sh')
-
-shutil.copyfile('include/lv_conf.h', 'lib/lvgl/lv_conf.h')

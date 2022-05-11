b76556566ebe3900d18d93483d262a487fdebf93
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2022-05-10 20:31:14 -0400

    create git_sha.h directly in python

--- scripts/pre_build.py
+++ scripts/pre_build.py
@@ -1,8 +1,27 @@
-import platform
+#!/usr/bin/env python3
 
-Import('env')
+import subprocess
+from pathlib import Path
 
-if platform.system() == 'Windows':
-    env.Execute('wsl sh scripts/get_sha.sh')
-else: # Linux
-    env.Execute('sh scripts/get_sha.sh')
+# get latest commit hash
+result = subprocess.run('git rev-parse --short HEAD', capture_output=True, text=True)
+version = result.stdout.strip()
+
+# check if the workspace is dirty
+result = subprocess.run('git diff --quiet', capture_output=True)
+if result.returncode > 0:
+    #workspace is dirty
+    version += '*'
+
+text = """
+#ifndef GIT_SHA
+#define GIT_SHA "{v}"
+#endif
+"""
+
+Path("lib/git_sha").mkdir(parents=True, exist_ok=True)
+
+with open('lib/git_sha/git_sha.h', 'w+') as fp:
+    fp.write(text.format(v=version))
+
+print(version)

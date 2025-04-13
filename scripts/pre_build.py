9a82b0a37fccc05e37e67f7b55fb033dba20b9e5
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-04-13 19:18:05 -0400

    fix pre_build script

--- scripts/pre_build.py
+++ scripts/pre_build.py
@@ -4,24 +4,26 @@ import subprocess
 from pathlib import Path
 
 # get latest commit hash
-result = subprocess.run('git rev-parse --short HEAD', capture_output=True, text=True)
+result = subprocess.run(
+    ["git", "rev-parse", "--short HEAD"], capture_output=True, text=True
+)
 version = result.stdout.strip()
 
 # check if the workspace is dirty
-result = subprocess.run('git diff --quiet', capture_output=True)
+result = subprocess.run(["git", "diff" "--quiet"], capture_output=True)
 if result.returncode > 0:
-    #workspace is dirty
-    version += '*'
+    # workspace is dirty
+    version += "*"
 
-text = """
+text = f"""
 #ifndef GIT_SHA
-#define GIT_SHA "{v}"
+#define GIT_SHA "{version}"
 #endif
 """
 
 Path("lib/git_sha").mkdir(parents=True, exist_ok=True)
 
-with open('lib/git_sha/git_sha.h', 'w+') as fp:
-    fp.write(text.format(v=version))
+with open("lib/git_sha/git_sha.h", "w+") as fp:
+    fp.write(text)
 
 print(version)

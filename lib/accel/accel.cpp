b6385328ef9ebd85413705e97018b99a7964a478
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-04-16 18:46:03 -0400

    calibrate gyro

--- lib/accel/accel.cpp
+++ lib/accel/accel.cpp
@@ -26,6 +26,9 @@ void init(void)
 
 void update(void)
 {
+    static float sum[3];
+    static uint32_t count;
+
     // Update accel values
     mpu.getEvent(&accel, &gyro, &temp);
 
@@ -35,9 +38,14 @@ void update(void)
     const float az = -accel.acceleration.x * 0.991911021f + 0.386845298f; // [-9.50,10.28]
 
     // correct gyro offset to minimize drift
-    const float gx = gyro.gyro.z + 0.01610f;
-    const float gy = gyro.gyro.y + 0.01491f;
-    const float gz = gyro.gyro.x + 0.01203f;
+    const float gx = gyro.gyro.z + 0.026756891f;
+    const float gy = gyro.gyro.y + 0.023081699f;
+    const float gz = gyro.gyro.x + 0.000516514f;
+
+    sum[0] += gx;
+    sum[1] += gy;
+    sum[2] += gz;
+    count += 1;
 
     const float deltat = fusion.deltatUpdate(); // this have to be done before calling the fusion update
     fusion.MahonyUpdate(                        // mahony is suggested if there isn't the mag and the mcu is slow
@@ -52,25 +60,39 @@ void update(void)
     if (millis() - last_tx > 100)
     {
         last_tx = millis();
-        Serial.print(ax);
-        Serial.print(',');
-        Serial.print(ay);
-        Serial.print(',');
-        Serial.print(az);
-        Serial.print(',');
-        Serial.print(gx);
-        Serial.print(',');
-        Serial.print(gy);
-        Serial.print(',');
-        Serial.print(gz);
-        Serial.print(',');
-        Serial.print(fusion.getYaw());
-        Serial.print(',');
-        Serial.print(fusion.getPitch());
-        Serial.print(',');
-        Serial.print(fusion.getRoll());
-        Serial.print(',');
-        Serial.print(temp.temperature);
+        if (false)
+        {
+            // gyro calibration
+            Serial.print(sum[0]);
+            Serial.print(',');
+            Serial.print(sum[1]);
+            Serial.print(',');
+            Serial.print(sum[2]);
+            Serial.print(',');
+            Serial.print(count);
+        }
+        else
+        {
+            Serial.print(ax);
+            Serial.print(',');
+            Serial.print(ay);
+            Serial.print(',');
+            Serial.print(az);
+            Serial.print(',');
+            Serial.print(gx);
+            Serial.print(',');
+            Serial.print(gy);
+            Serial.print(',');
+            Serial.print(gz);
+            Serial.print(',');
+            Serial.print(fusion.getYaw());
+            Serial.print(',');
+            Serial.print(fusion.getPitch());
+            Serial.print(',');
+            Serial.print(fusion.getRoll());
+            Serial.print(',');
+            Serial.print(temp.temperature);
+        }
         Serial.println();
     }
 }
